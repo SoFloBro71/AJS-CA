2265467f123ca165564ddace165167c10c3114ed
SoFloBro71 <n00211013@iadt.ie>
2025-01-29 09:46:36 +0000

    test

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/views/'))
 app.use((request, response, next) => {
     console.log('Time:', Date.now());
     next();
-});
+}); 
 
 ////// AUTHORIZATION ///////////////////////
 
